98f215e3167921347076ed83d87e14a0f75ca6be
Zane Werle <zanewerle@gmail.com>
2023-04-10 23:53:08 -0500

    added in the fetch request and started playing around with the api link. None of the vars are necessarily here to stay yet, but we wanted to make sure we could manipulate the url as needed.

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var requestUrl = "https://api.rawg.io/api/platforms?key=ac7de14847e84d37be3b60940720db8c"
+var gamesUrl = "https://api.rawg.io/api/games?key=ac7de14847e84d37be3b60940720db8c&dates=2019-09-01,2019-09-30&platforms=1"
+var testUrl = "https://api.rawg.io/api/games?key=ac7de14847e84d37be3b60940720db8c&parent_platforms=" + choice1 + "&genres=" + choice2
+var choice1 = 187 //Playstation 5 (playstation, xbox, nintendo)
+var choice2 = "2, 3, 4" //action or shooter
+var testUrl = "https://api.rawg.io/api/games?key=ac7de14847e84d37be3b60940720db8c&platforms=" + choice1 + "&genres=" + choice2
+
+
+fetch(testUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+  
